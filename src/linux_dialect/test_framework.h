6dc12e6a0a9a1602cab9652b69c3f142edcf76b4
Alex V. Podolsky <apodolsk@andrew.cmu.edu>
2015-10-31 16:15:40 -0400

    Switched to submodules.

--- src/linux_dialect/test_framework.h
+++
@@ -1,8 +0,0 @@
-extern cnt nthreads;
-
-void launch_test(void *test(void *), const char *name);
-void thr_sync(void);
-
-err pause_universe(void);
-void resume_universe(void);
-void wait_for_universe();
